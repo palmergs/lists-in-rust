f379f4411fb244830afe959a1c51f59a6aabca7e
Galen Palmer <palmergs@gmail.com>
2017-06-10 11:54:46 -0400

    join all List fn under single impl

--- src/second.rs
+++ src/second.rs
@@ -49,15 +49,21 @@ impl<T> List<T> {
             &mut node.elem
         })
     }
-}
 
-pub struct IntoIter<T>(List<T>);
-
-impl<T> List<T> {
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut { next: self.head.as_mut().map(|node| &mut **node) }
+    }
 }
+
+pub struct IntoIter<T>(List<T>);
 
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
@@ -68,12 +74,6 @@ impl<T> Iterator for IntoIter<T> {
 
 pub struct Iter<'a, T: 'a> {
     next: Option<&'a Node<T>>,
-}
-
-impl<T> List<T> {
-    pub fn iter(&self) -> Iter<T> {
-        Iter { next: self.head.as_ref().map(|node| &**node) }
-    }
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
@@ -88,12 +88,6 @@ impl<'a, T> Iterator for Iter<'a, T> {
 
 pub struct IterMut<'a, T: 'a> {
     next: Option<&'a mut Node<T>>,
-}
-
-impl<T> List<T> {
-    pub fn iter_mut(&mut self) -> IterMut<T> {
-        IterMut { next: self.head.as_mut().map(|node| &mut **node) }
-    }
 }
 
 impl<'a, T> Iterator for IterMut<'a, T> {
